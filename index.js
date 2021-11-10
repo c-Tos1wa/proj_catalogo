a836fbeb2a5e9d383fa7eeaad65be0413d7e4ad9
Cristina <cristoshiwassaki@gmail.com>
2021-11-09 23:28:45 -0300

    configurando o projeto

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const port = 3000;
+
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+app.get("/", (req, res) => {
+  res.render('index');
+});
+
+app.listen(port, () => console.log(`Servidor operando em http://localhost:${port}`))
